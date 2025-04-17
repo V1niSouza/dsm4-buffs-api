bc5f009255528e0cd913d186ffcb77f5459cbe18
unknown <viniciussouzaramospv@gmail.com>
2025-04-17 10:41:03 -0300

    feat: adicionando arquivos basicos para inicialização do serviço

---
+++ index.js
@@ -0,0 +1,8 @@
+// Definindo porta que a API vai rodar, no caso 4000
+const port = 4000;
+app.listen(port, (error) => {
+    if(error) {
+        console.log(error);
+    }
+    console.log(`API rodando em http://localhost:${port}.`);
+})
