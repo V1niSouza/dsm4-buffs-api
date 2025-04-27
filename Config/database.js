4a7a4392ce5fadd0fce8d4c3e79ab2cc5f1c7253
unknown <viniciussouzaramospv@gmail.com>
2025-04-27 20:17:08 -0300

    chore: passando o banco para nuvem

--- Config/database.js
+++ Config/database.js
@@ -6,11 +6,11 @@ mongoose.connect(process.env.MONGO_URI);
 const connection = mongoose.connection;
 
 connection.on("error", () => {
-  console.log("❌ Erro ao conectar com o MongoDB local");
+  console.log("Erro ao conectar com o MongoDB Atlas");
 });
 
 connection.once("open", () => {
-  console.log("✅ Conectado ao MongoDB local com sucesso!");
+  console.log("Conectado ao MongoDB Atlas!");
 });
 
 export default mongoose;
